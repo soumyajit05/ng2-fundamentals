2f3d5e456879acd0fe4f2542e522ca02ce4396cf
Jit <soumyajit.ray@capitalone.com>
2017-09-13 23:23:59 -0400

    sidebar nav added

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { eventAppComponent } from './events-app.component';
 import { eventListComponent } from './event-list/event-list.component';
 import { eventThumbnailComponent } from './event-list/event-thumbnail.component';
+import { navbarComponent } from './nav/navbar.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [eventAppComponent, eventListComponent, eventThumbnailComponent],
+    declarations: [eventAppComponent, eventListComponent, eventThumbnailComponent, navbarComponent],
     bootstrap: [eventAppComponent]
 })
 
--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'events-app',
     //template: `<h1>Event App Component loaded</h1>`
-    template: '<event-list></event-list>'
+    template: `
+                <nav-bar></nav-bar>
+                <event-list></event-list>`
 })
 
 export class eventAppComponent implements OnInit {
