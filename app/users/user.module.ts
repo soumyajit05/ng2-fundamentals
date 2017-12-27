6df2b340aec7d9b2bc2ea879f8b9a1fe155b099e
Jit <soumyajit.ray@capitalone.com>
2017-12-27 16:28:48 -0500

    ngForm Directive

--- app/users/user.module.ts
+++ app/users/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile.component';
 import { RouterModule } from '@angular/router';
 import { UserRoutes } from './user.routes';
+import { loginComponent } from './login.component';
 
 @NgModule({
-    imports: [CommonModule, RouterModule.forChild(UserRoutes)],
-    declarations: [ProfileComponent]
+    imports: [CommonModule, RouterModule.forChild(UserRoutes), FormsModule],
+    declarations: [ProfileComponent, loginComponent]
 })
 
 export class UserModule { }
--- app/users/user.routes.ts
+++ app/users/user.routes.ts
@@ -1,5 +1,7 @@
 import { ProfileComponent } from './profile.component';
+import { loginComponent } from './login.component';
 
 export const UserRoutes = [
-    { path: 'profile', component: ProfileComponent }
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: loginComponent }
 ]
