601121a40e9e6bfd116d753113a41ecc1202442e
Jit <ray.soumyajit24@gmail.com>
2018-01-12 09:12:32 -0500

    Validate reactive form -1

--- app/users/profile.component.ts
+++ app/users/profile.component.ts
@@ -1,36 +1,64 @@
 import { Component, OnInit } from "@angular/core";
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 import { FormControl, FormGroup } from "@angular/forms";
-import {AuthService} from "./auth-service";
+import { AuthService } from "./auth-service";
+import { Validators } from "@angular/forms";
 
 @Component({
-    selector: `profile-component`,
-    templateUrl:'app/users/profile.component.html'
+  selector: `profile-component`,
+  templateUrl: 'app/users/profile.component.html'
 })
 
 export class ProfileComponent implements OnInit {
 
-    lastName:any;
-    firstName:any;
-    profileFormGroup:FormGroup;
+  private lastName: FormControl;
+  private firstName: FormControl;
+  profileFormGroup: FormGroup;
 
-    constructor(private router:Router,private authService:AuthService) { }
-    ngOnInit() { 
-      this.lastName = new FormControl(this.authService.user.lastName);
-      this.firstName = new FormControl(this.authService.user.firstName);
+  constructor(private router: Router, private authService: AuthService) { }
+  ngOnInit() {
+    this.lastName = new FormControl(this.authService.user.lastName, Validators.required);
+    this.firstName = new FormControl(this.authService.user.firstName, Validators.required);
 
-      this.profileFormGroup = new FormGroup({
-        lastName:this.lastName,
-        firstName:this.firstName
-      });
+    this.profileFormGroup = new FormGroup({
+      lastName: this.lastName,
+      firstName: this.firstName
+    });
+  }
+
+  validateLastName() {
+    if (this.lastName.invalid || this.lastName.untouched) {
+      // console.log(`lastname is dirty`);
+      return false;
     }
+    else {
+      // console.log(`lastname is not that dirty`);
+      return true;
+    }
+  }
 
-    Save()
-    {
+  validateFirstName() {
+    if (!(this.firstName.invalid || this.firstName.untouched)) {
+      // console.log(`firstName is dirty`);
+      return true;
+    }
+    else {
+      // console.log(`firstName is not that dirty`);
+      return false;
+    }
+  }
+
+  Save() {
+    if (this.profileFormGroup.valid) {
+      // console.log('Form is valid');
       this.router.navigate(['events']);
     }
-
-    Cancel(){
-      this.router.navigate(['events']);
+    else {
+      // console.log(`missing entries`);
     }
+  }
+
+  Cancel() {
+    this.router.navigate(['events']);
+  }
 }
